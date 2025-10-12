a90bff37ca0e6763ed99e6d1b8fa9e665515cdad
Sparsh2509 <sparshgupta250904@gmail.com>
2025-10-12 13:49:27 +0530

    Merge file code completetd

--- Merge_dataset.py
+++ Merge_dataset.py
@@ -14,3 +14,15 @@ true["label"] = 1
 # Taking 10000 random rows from both dataset
 fake_sample = fake.sample(10000, random_state=42)
 true_sample = true.sample(10000, random_state=42)
+
+final_df = pd.concat([fake_sample, true_sample])
+
+# Shuffle rows (to mix fake & true)
+final_df = final_df.sample(frac=1, random_state=42).reset_index(drop=True)
+
+# Save to CSV file
+final_df.to_csv("final_news_dataset.csv", index=False)
+
+print("final_news_dataset.csv created successfully!")
+print("Total rows:", len(final_df))
+print(final_df.head())
