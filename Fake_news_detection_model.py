26a9a477b98afb317f35cd2e4626ed6489fc6193
Sparsh2509 <sparshgupta250904@gmail.com>
2025-10-12 19:11:42 +0530

    Model file code completed

--- Fake_news_detection_model.py
+++ Fake_news_detection_model.py
@@ -5,6 +5,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import os
 import joblib
 
 # Load cleaned dataset 
@@ -40,8 +41,6 @@ print(classification_report(y_test, y_pred, target_names=['Fake', 'Real']))
 print("Confusion Matrix:")
 print(confusion_matrix(y_test, y_pred))
 
-import os
-import joblib
 
 # Create folder if it doesn't exist
 model_dir = r'D:\Sparsh\ML_Projects\Fake_News_Detection\Model'
@@ -49,9 +48,9 @@ os.makedirs(model_dir, exist_ok=True)  # creates folder if missing
 
 # Save Naive Bayes model
 joblib.dump(nb_model, os.path.join(model_dir, 'nb_fake_news_model.joblib'))
-print("✅ Naive Bayes model saved successfully.")
+print("Naive Bayes model saved successfully.")
 
 # Save TF-IDF vectorizer
 joblib.dump(tfidf, os.path.join(model_dir, 'tfidf_vectorizer.joblib'))
-print("✅ TF-IDF vectorizer saved successfully.")
+print("TF-IDF vectorizer saved successfully.")
 
