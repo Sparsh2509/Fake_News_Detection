d23dc95249251f4a38da2fa065f2cdc15e5d0c17
Sparsh2509 <sparshgupta250904@gmail.com>
2025-10-12 17:08:26 +0530

    NLTK resources download

--- NLP_Dataset_preprocessing.py
+++ NLP_Dataset_preprocessing.py
@@ -3,3 +3,9 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+
+# Download NLTK resources (only once)
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+
